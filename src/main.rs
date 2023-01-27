1f81aec229b5ec2acc0e45795116cb6a60a19509
Christian Albert <christianalbert@users.noreply.github.com>
2023-01-27 13:24:13 +0000

    Some initial steps, including rocket rc dependencies

--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,11 @@
-fn main() {
-    println!("Hello, world!");
+#[macro_use] extern crate rocket;
+
+#[get("/")]
+fn index() -> &'static str {
+    "Hello, world!"
 }
 
-// fucntion that returns the square of a number
-fn square(x: i32) -> i32 {
-    x * x
+#[launch]
+fn rocket() -> _ {
+    rocket::build().mount("/", routes![index])
 }
-
-// 
-// Path: src/lib.rs
-#[cfg(test)]
-mod tests {
-
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
-
-    #[test]
-    fn test_square() {
-       assert_eq!(square(2), 4);
-       assert_eq!(square(3), 9);
-       assert_eq!(square(4), 16);
-       assert_eq!(square(5), 25);
-       assert_eq!(square(6), 36);
-       
-    }
-}
-
